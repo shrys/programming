003800cdee6c606260c604329269b6e2fbf4f857
shrys <shreyas770@yahoo.in>
2020-09-06 23:36:49 +0530

    HackerRank

---
+++ hackerrank/10day_statistics/day0/2WeightedMean.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int WeightedMean()
+{
+    vector<int> x, w;
+    int n, t, xw = 0, s = 0;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> t, x.push_back(t);
+    for (int i = 0; i < n; i++)
+        cin >> t, w.push_back(t), xw += t * x[i], s += t;
+
+    printf("%.1f", (float)xw / s);
+	return 0;
+}
